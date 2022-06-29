0f9ff55780d3102440a06ad936102bee4d8ae4de
HassanAlsamahi <hassanalsamahi12@gmail.com>
2022-06-29 10:42:11 +0200

    closes #9: Apply - function for cd

--- main.go
+++ main.go
@@ -7,10 +7,11 @@ import (
 	"os"
 	"os/exec"
 	"os/user"
+	"strconv"
 	"strings"
 )
 
-func execInput(input string) error {
+func execInput(input string, work_dir_cache map[int]string, counter int) (error, map[int]string, int) {
 
 	input = strings.TrimSuffix(input, "\n")
 
@@ -26,29 +27,62 @@ func execInput(input string) error {
 		os.Exit(0)
 	case "cd":
 		home_dir, _ := os.UserHomeDir()
-		if len(args) < 2 {
-			return os.Chdir(home_dir)
+		if len(args) < 2 || args[1] == "" {
+			counter += 1
+			work_dir_cache[counter] = home_dir
+			return os.Chdir(home_dir), work_dir_cache, counter
+		} else if args[1] == "-" {
+			if len(work_dir_cache) < 2 {
+				fmt.Println("There is no old working directory")
+				return nil, work_dir_cache, counter
+			}
+			arg_int := counter
+			old_work_dir := work_dir_cache[counter]
+			if len(args) > 2 {
+				arg_int, _ = strconv.Atoi(args[2])
+				old_work_dir = work_dir_cache[arg_int]
+			} else {
+				old_work_dir = work_dir_cache[arg_int-1]
+			}
+			counter += 1
+			work_dir_cache[counter] = old_work_dir
+			return os.Chdir(old_work_dir), work_dir_cache, counter
 		} else {
-			return os.Chdir(args[1])
+			if os.Chdir(args[1]) == nil {
+				counter += 1
+				work_dir_cache[counter] = args[1]
+
+			}
+			return os.Chdir(args[1]), work_dir_cache, counter
 		}
 
 	case "\f":
 		args[0] = "clear"
+	case "wdcache":
+		fmt.Println(work_dir_cache)
+		return nil, work_dir_cache, counter
 	}
 
-	return cmd.Run()
+	return cmd.Run(), work_dir_cache, counter
 }
 
 func main() {
 	fmt.Println("Hello to Gshell!!")
+	homedir, _ := os.UserHomeDir()
+	if err := os.Chdir(homedir); err != nil {
+
+	}
+
+	workDir, _ := os.Getwd()
+	counter := 1
+	work_dir_cache := map[int]string{counter: workDir}
 
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		hostname, _ := os.Hostname()
-		workDir, _ := os.Getwd()
 		user, _ := user.Current()
 
-		fmt.Printf("[%s~%s~%s]> ", user.Username, hostname, workDir)
+		fmt.Printf("[%s~%s~%s]> ", user.Username, hostname, work_dir_cache[counter])
 
 		input, err := reader.ReadString('\n')
 		if err == io.EOF {
@@ -58,7 +92,7 @@ func main() {
 			fmt.Fprintln(os.Stderr, err)
 		}
 
-		if err = execInput(input); err != nil {
+		if err, work_dir_cache, counter = execInput(input, work_dir_cache, counter); err != nil {
 			if err.Error() == io.EOF.Error() {
 				os.Exit(0)
 			} else if err.Error() != "exec: no command" {
@@ -66,5 +100,6 @@ func main() {
 			}
 
 		}
+		//fmt.Println(work_dir_cache)
 	}
 }
